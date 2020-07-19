380752f78d70a98fc26fd7d9bf8d25313f3f909c
Codoffer <codoffer@gmail.com>
2020-07-19 15:26:49 +0530

    Router Seperation

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const app = express();
 const config = require('config');
 const debug = require('debug')('app:init');
 
+const defaultRoutes = require('./routes/default');
+const courseRoutes = require('./routes/courses');
+
+//Get environment
+//const environment = app.get('env');
+
 app.set('view engine', 'pug');
 app.set('views', './views');
 
@@ -10,49 +16,19 @@ app.set('views', './views');
 console.log('Application Name ' + config.get('name'));
 
 debug('Application has been started');
-debug('Raju has been worked');
+//debug('Raju has been worked');
 
-app.use((req, res, next) => {
+//Custom Middleware
+/* app.use((req, res, next) => {
   console.log('logging');
   next();
-});
+}); */
 
-const courses = [
-  { id: 1, name: 'Course 1' },
-  { id: 2, name: 'Course 2' },
-  { id: 3, name: 'Course 3' }
-];
-
-app.get('/', (req, res) => {
-  res.render('index', {
-    title: 'Express Application',
-    heading: 'Hello World Heading'
-  })
-  //res.send('Hello World - is here !!!');
-});
-
-app.get('/api/courses', (req, res) => {
-  res.send([1, 2, 3, 4]);
-});
-
-app.get('/api/courses/:id', (req, res) => {
-  const course = courses.find((c) => c.id == parseInt(req.params.id));
-  if (!course) res.status(404).send('No course found');
-  res.send(course);
-});
+app.use('/', defaultRoutes);
+app.use('/api/courses', courseRoutes);
 
 const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
   console.log(`Listening port on ${port} ...`);
 });
-
-/* const Logger = require('./logger');
-
-const logger = new Logger();
-
-logger.on('messageListner', (arg) => {
-    console.log('Message Listner : ', arg);
-});
-
-logger.showMessage('Test'); */
